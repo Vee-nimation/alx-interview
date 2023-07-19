f6ece10d58006c8de10f857815c6515a2cf5e728
Vee-nimation <vincentakporhuarho@gmail.com>
2023-07-19 07:28:55 +0000

    frontend

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,44 @@
 #!/usr/bin/python3
-
+"""Log Parser"""
 import sys
-from collections import defaultdict
 
-def print_statistics(file_sizes, status_counts):
-    total_size = sum(file_sizes)
-    print(f"Total file size: File size: {total_size}")
-    for status_code, count in sorted(status_counts.items()):
-        print(f"{status_code}: {count}")
 
-def process_input():
-    file_sizes = []
-    status_counts = defaultdict(int)
-    line_count = 0
+if __name__ == '__main__':
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
+
+    def parse_line(line):
+        """ Checks the line for matches """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
+    linenum = 1
     try:
         for line in sys.stdin:
-            line_count += 1
-
-            # Parse the line in the given format
-            parts = line.split()
-            if len(parts) != 7 or not parts[3].isdigit():
-                continue
-
-            _, _, _, _, status_code, file_size, _ = parts
-
-            # Check if status_code is valid and add it to status_counts
-            try:
-                status_code = int(status_code)
-                if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-                    status_counts[status_code] += 1
-            except ValueError:
-                pass
-
-            # Add the file size to file_sizes
-            try:
-                file_size = int(file_size)
-                file_sizes.append(file_size)
-            except ValueError:
-                pass
-
-            # Print statistics every 10 lines
-            if line_count % 10 == 0:
-                print_statistics(file_sizes, status_counts)
-
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
     except KeyboardInterrupt:
-        pass
-
-    # Print final statistics
-    print_statistics(file_sizes, status_counts)
-
-if __name__ == "__main__":
-    process_input()
+        print_stats()
+        raise
+    print_stats()
